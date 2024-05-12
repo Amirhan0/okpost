ecbe861cfa12f3f4e6e5eec1d5130e6c781ab053
Amirhan0 <amirgasanov246@gmail.com>
2024-05-12 22:52:43 +0600

    profileConfirmPassword

--- profile/profile.js
+++ profile/profile.js
@@ -34,5 +34,101 @@ document.addEventListener("DOMContentLoaded", function () {
   emailElement.textContent = localStorage.getItem("E-mail");
   phoneNumberElement.textContent = localStorage.getItem("Телефон");
   adresElement.textContent = localStorage.getItem("Адрес");
-}); 
-  
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const avatarStorage = localStorage.getItem("Аватарка (ссылка)");
+  const nameStorage = localStorage.getItem("Логин");
+
+  if (nameStorage && avatarStorage) {
+    const authLink = document.querySelector(".name .top__name");
+    authLink.textContent = nameStorage;
+
+    const userImage = document.querySelector(".avatar__name .avatar");
+    userImage.src = avatarStorage;
+    userImage.style.width = "60px";
+    userImage.style.width = "50px";
+    userImage.style.borderRadius = "30px";
+
+    const deleteBtn = document.createElement("text");
+    deleteBtn.classList.add("delete-txt");
+    deleteBtn.textContent = "Выйти";
+
+    deleteBtn.addEventListener("click", () => {
+      localStorage.removeItem("Логин");
+      localStorage.removeItem("Аватарка (ссылка)");
+
+      location.reload();
+    });
+    const authDiv = document.querySelector(".avatar__name");
+    authDiv.appendChild(deleteBtn);
+  }
+});
+
+const modal = document.getElementById("myModal");
+const openModalBtn = document.querySelector(".change__password");
+const closeModalBtn = document.getElementsByClassName("close")[0];
+const changePasswordBtn = document.getElementById("changePasswordBtn");
+
+openModalBtn.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeModalBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+const url = "https://3d97fdc2ac6a904f.mokky.dev/reg";
+changePasswordBtn.addEventListener("click", async () => {
+  const old = document.querySelector("#oldPassword");
+  const oldPassword = old.value;
+  const newpass = document.querySelector("#newPassword");
+  const newPassword = newpass;
+  const confirmpass = document.querySelector("#confirmPassword");
+
+  const confirmPassword = confirmpass.value;
+
+  if (newPassword !== confirmPassword) {
+    showError(newpass, "Пароли должны совпадать");
+    showError(confirmpass, "Пароли должны совпадать");
+  } else {
+    hideError(newpass);
+    hideError(confirmpass);
+  }
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ oldPassword, newPassword }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Ошибка при изменении пароля");
+    }
+    alert("Пароль успешно изменен!");
+    modal.style.display = "none";
+  } catch (error) {
+    console.error("Ошибка:", error);
+    alert("Произошла ошибка при изменении пароля" + error.message);
+  }
+});
+
+function showError(input, message) {
+  const errorLabel = input.nextElementSibling;
+  errorLabel.textContent = message;
+  errorLabel.style.display = "block";
+}
+
+function hideError(input) {
+  const errorLabel = input.nextElementSibling;
+  errorLabel.textContent = " ";
+  errorLabel.style.display = "none";
+}
