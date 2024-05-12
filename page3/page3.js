069df3307c3317c525cd218288dcbd426123dc0f
Amirhan0 <amirgasanov246@gmail.com>
2024-05-12 20:49:33 +0600

    localFIX

--- page3/page3.js
+++ page3/page3.js
@@ -1,3 +1,19 @@
-document.querySelector('.logo__text').addEventListener('click', () => {
-  window.location.href = '../index.html'
-})
+document.querySelector(".logo__text").addEventListener("click", () => {
+  window.location.href = "../index.html";
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const avatarStorage = localStorage.getItem("Аватарка (ссылка)");
+  const nameStorage = localStorage.getItem("Логин");
+
+  if (nameStorage && avatarStorage) {
+    const authLink = document.querySelector(".auth a");
+    authLink.textContent = nameStorage;
+
+    const userImage = document.querySelector(".auth .user");
+    userImage.src = avatarStorage;
+    userImage.style.width = "58px";
+    userImage.style.width = "58px";
+    userImage.style.borderRadius = "50%";
+  }
+});
